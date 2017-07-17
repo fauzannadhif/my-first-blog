0e4e87bad900539bceb696eed65f71c8be9cf146
nadhif <fauzannadhifm98@gmail.com>
2017-07-17 14:36:04 +0700

    üpdate

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')^_inwg-sg-g&$0km%vszcm*k@qf3h8^!j7=g9t0qf8ozm=q8l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'fauzannadhif.pythonanywhere.com']
 
 
 # Application definition
